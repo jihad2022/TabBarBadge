b815936b3eb3e07aa6dc8da0866a7558f78837d9
Jihad Mahmoud <jihad.fayz@gmail.com>
2019-11-06 01:08:10 +0200

    Initial Commit

---
+++ TabBarBadge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabBarBadge
+//
+//  Created by Syber Experties on 06/11/2019.
+//  Copyright © 2019 Jihad iOS. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
